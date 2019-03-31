5305744b73d3392cb2c9c655d460569e67e947b9
Ryan Grunest <ryangrunest45@gmail.com>
2019-03-31 00:20:43 -0700

    created letter constructor

--- letter.js
+++ letter.js
@@ -1,13 +1,19 @@
-class Letter {
-    constructor(char, guessed) {
-        this.char = char;
-        this.guessed = guessed;
-        guessedCorrectly = function() {
-            if (this.guessed === true) {
-                return this.char;
-            } else {
-                return '_';
-            }
+function Letter(char, guessed) {
+    this.char = char;
+    this.guessed = guessed;
+    this.guessedCorrectly = function() {
+        if (this.guessed === true) {
+            return this.char;
+        } else {
+            return '_';
+        }
+    };
+    this.checkGuess = function(letter) {
+        if (letter === this.char) {
+            this.guessed = true;
+            this.guessedCorrectly();
         }
     }
-}
+};
+let check = new Letter('a', false);
+console.log(check);
